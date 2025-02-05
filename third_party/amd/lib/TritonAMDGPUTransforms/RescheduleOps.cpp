82a49eb668908ed3892a1ed080027030fd1ce3c0
ravil-mobile <ravil.aviva.com@gmail.com>
2025-02-05 14:18:46 +0000

    [AMD] Added a proto of the dependency-graph

--- third_party/amd/lib/TritonAMDGPUTransforms/RescheduleOps.cpp
+++ third_party/amd/lib/TritonAMDGPUTransforms/RescheduleOps.cpp
@@ -21,118 +21,19 @@
 #define LDBG(X) LLVM_DEBUG(DBGS() << X << "\n")
 
 namespace {
-
-// TODO (ravil): Note, took function from `SchedInstructions.cpp`.
-// we need to combine these two implementations
-Operation *createSchedBarrier(OpBuilder &rewriter, Location loc,
-                              mlir::amdgpu::sched_barrier_opt_enum maskValue) {
-  IntegerAttr mask =
-      rewriter.getI32IntegerAttr(static_cast<int32_t>(maskValue));
-  return rewriter.create<ROCDL::SchedBarrier>(loc, mask);
-}
-
 struct Node {
-  Node(Operation *op, int32_t weight = 0) : op(op), weight(weight) {}
+  Node(Operation *op) : op(op) {}
   enum class ChildType { Real, Artificials };
-
-  Operation *getOp() { return op; }
-  int32_t getWeight() { return weight; }
-  void setWeight(int32_t priority) { this->weight = priority; }
 
   template <ChildType Type> void add(Node *node) {
     llvm::SetVector<Node *> *children =
         Type == ChildType::Real ? &realChildren : &artificialChildren;
     children->insert(node);
   }
-  void addParent(Node *node) { parents.insert(node); }
 
-  size_t getNumParents() { return parents.size(); }
-  bool hasChildren() {
-    return !(realChildren.empty() && artificialChildren.empty());
-  }
-  bool hasNoChildren() { return !hasChildren(); }
-
-  const llvm::SetVector<Node *> &getRealChildren() { return realChildren; }
-  const llvm::SetVector<Node *> &getArtificialChildren() {
-    return artificialChildren;
-  }
-  const llvm::SetVector<Node *> &getParents() { return parents; }
-
-  void removeChild(Node *node) {
-    if (realChildren.contains(node)) {
-      realChildren.remove(node);
-    }
-    if (artificialChildren.contains(node)) {
-      artificialChildren.remove(node);
-    }
-  }
-
-  void removeParent(Node *node) {
-    if (parents.contains(node)) {
-      parents.remove(node);
-    }
-  }
-
-  void drainChildren() {
-    realChildren.clear();
-    artificialChildren.clear();
-  }
-
-private:
   Operation *op;
-  int32_t weight;
   llvm::SetVector<Node *> realChildren;
   llvm::SetVector<Node *> artificialChildren;
-  llvm::SetVector<Node *> parents;
-};
-
-struct NodeWeightStrategy {
-  virtual void set(llvm::SmallVector<std::unique_ptr<Node>> &nodes) = 0;
-};
-
-struct BasicDotWeightStrategy : public NodeWeightStrategy {
-  void set(llvm::SmallVector<std::unique_ptr<Node>> &nodes) override {
-    SmallVector<Node *> dots;
-    SmallVector<Node *> loads;
-    for (auto &node : nodes) {
-      if (dyn_cast<triton::DotOp>(node->getOp()))
-        dots.push_back(node.get());
-      if (dyn_cast<triton::LoadOp>(node->getOp()))
-        loads.push_back(node.get());
-    }
-
-    // Make sure that prio is never equal to `0` for dotOps
-    constexpr int32_t extraDefaultWeight = 10;
-    int32_t dotWeight = dots.size() + extraDefaultWeight;
-    const int32_t loadWeight = dotWeight + extraDefaultWeight;
-    for (auto loadNode : loads) {
-      propagate(loadNode, loadWeight);
-    }
-
-    for (auto dotNode : dots) {
-      propagate(dotNode, dotWeight--);
-    }
-  }
-
-private:
-  void propagate(Node *node, int32_t weight) {
-    if (visited.contains(node))
-      return;
-
-    node->setWeight(weight);
-    visited.insert({node});
-
-    if (node->hasChildren()) {
-      for (auto child : node->getRealChildren()) {
-        propagate(child, weight);
-      }
-      for (auto child : node->getArtificialChildren()) {
-        propagate(child, weight);
-      }
-    }
-  }
-
-  SetVector<Node *> visited{};
 };
 
 struct Graph {
@@ -142,136 +43,70 @@ public:
     createEdges();
   }
 
-  Graph(const Graph &other) {
-    using iteratorType = decltype(other.nodes.begin());
-    DenseMap<Node *, iteratorType> map;
-    for (auto it = other.nodes.begin(); it != other.nodes.end(); ++it) {
-      auto newNode =
-          std::make_unique<Node>(it->get()->getOp(), it->get()->getWeight());
-      map.insert({it->get(), it});
-      lookup.insert({newNode->getOp(), newNode.get()});
-      nodes.push_back(std::move(newNode));
-    }
-
-    for (auto [idx, otherNode] : llvm::enumerate(other.nodes)) {
-      auto &currNode = nodes[idx];
-      for (auto otherChild : otherNode->getRealChildren()) {
-        auto otherChildIt = map.find(otherChild)->second;
-        auto childIdx = std::distance(other.nodes.begin(), otherChildIt);
-        currNode->add<Node::ChildType::Real>(nodes[childIdx].get());
-      }
-
-      for (auto otherChild : otherNode->getArtificialChildren()) {
-        auto otherChildIt = map.find(otherChild)->second;
-        auto childIdx = std::distance(other.nodes.begin(), otherChildIt);
-        currNode->add<Node::ChildType::Artificials>(nodes[childIdx].get());
-      }
-
-      for (auto otherParent : otherNode->getParents()) {
-        auto otherParentIt = map.find(otherParent)->second;
-        auto parentIdx = std::distance(other.nodes.begin(), otherParentIt);
-        currNode->addParent(nodes[parentIdx].get());
-      }
-      nodes.push_back(std::make_unique<Node>(otherNode->getOp()));
-    }
-  }
-
-  SmallVector<Node *> getNodes() {
-    SmallVector<Node *> copy(nodes.size(), nullptr);
-    for (auto [idx, node] : llvm::enumerate(nodes)) {
-      copy[idx] = node.get();
-    }
-    return copy;
-  }
-
-  void setNodesWeights(NodeWeightStrategy &&strategy) { strategy.set(nodes); }
+  llvm::SmallVector<std::unique_ptr<Node>> &getNodes() { return nodes; }
+  SmallVector<SetVector<Node *>> &getBlocks() { return blocks; }
 
 private:
+  SetVector<Node *> *getBlock(Node *node) {
+    for (auto &block : blocks) {
+      if (block.contains(node))
+        return &block;
+    }
+    return nullptr;
+  }
+
   void createNodes(Block *mlirBlock) {
+    SetVector<Node *> currBlock;
     for (auto it = mlirBlock->begin(); it != mlirBlock->end(); ++it) {
       Operation *op = &(*it);
       std::unique_ptr<Node> node = std::make_unique<Node>(op);
+      currBlock.insert(node.get());
       lookup.insert({op, node.get()});
       nodes.push_back(std::move(node));
-    }
-  }
 
-  enum class Traversal { Topdown, Bottomup };
-  template <Traversal Direction> void insertGPUBarrierEdges() {
-
-    auto fwIt = nodes.begin();
-    auto bkIt = nodes.rbegin();
-    auto next = [&]() -> Node * {
-      if constexpr (Direction == Traversal::Topdown) {
-        if (fwIt == nodes.end())
-          return nullptr;
-        return (fwIt++)->get();
-      }
-      if constexpr (Direction == Traversal::Bottomup) {
-        if (bkIt == nodes.rend())
-          return nullptr;
-        return (bkIt++)->get();
-      }
-      return nullptr;
-    };
-
-    llvm::SmallVector<Node *> ldsOpsNodes;
-    while (Node *node = next()) {
-      auto localLoad = dyn_cast<triton::gpu::LocalLoadOp>(node->getOp());
-      auto localStore = dyn_cast<triton::gpu::LocalStoreOp>(node->getOp());
-      auto localAlloc = dyn_cast<triton::gpu::LocalAllocOp>(node->getOp());
-      if (localLoad || localStore || localAlloc) {
-        ldsOpsNodes.push_back(node);
-      }
-      auto gpuBarrier = dyn_cast<mlir::gpu::BarrierOp>(node->getOp());
-      if (gpuBarrier) {
-        Node *barrierNode = node;
-        for (auto ldsOpNode : ldsOpsNodes) {
-          if constexpr (Direction == Traversal::Topdown) {
-            barrierNode->add<Node::ChildType::Artificials>(ldsOpNode);
-            ldsOpNode->addParent(barrierNode);
-          }
-          if constexpr (Direction == Traversal::Bottomup) {
-            barrierNode->addParent(ldsOpNode);
-            ldsOpNode->add<Node::ChildType::Artificials>(barrierNode);
-          }
+      if (auto barrier = llvm::dyn_cast<mlir::gpu::BarrierOp>(op)) {
+        if (!currBlock.empty()) {
+          blocks.push_back(std::move(currBlock));
+          currBlock = SetVector<Node *>{};
         }
-        ldsOpsNodes.clear();
       }
     }
+    if (!currBlock.empty())
+      blocks.push_back(std::move(currBlock));
   }
 
   void createEdges() {
-    // insert edges imposed by def-use chains
-    for (auto it = nodes.rbegin(); it != nodes.rend(); ++it) {
-      auto &node = *it;
-      for (auto operandValue : node->getOp()->getOperands()) {
-        auto operandDefOp = operandValue.getDefiningOp();
-        if (!lookup.contains(operandDefOp))
-          continue;
-        Node *childNode = lookup.find(operandDefOp)->second;
-        node->add<Node::ChildType::Real>(childNode);
-        childNode->addParent(node.get());
-      }
-    }
-
-    // gpu.Barrier ops are orphans. Add edges to
-    // respect data dependencies in the block
-    insertGPUBarrierEdges<Traversal::Bottomup>();
-    insertGPUBarrierEdges<Traversal::Topdown>();
-
-    // connect orphans with the last op in the block
-    auto &lastNode = *(nodes.rbegin());
-    for (auto it = std::next(nodes.rbegin()); it != nodes.rend(); ++it) {
-      auto &node = *it;
-      if (node->getNumParents() == 0) {
-        node->addParent(lastNode.get());
-        lastNode->add<Node::ChildType::Artificials>(node.get());
+    for (auto blockIt = blocks.rbegin(); blockIt != blocks.rend(); ++blockIt) {
+      auto &currBlock = *blockIt;
+      for (auto nodeIt = currBlock.rbegin(); nodeIt != currBlock.rend();
+           ++nodeIt) {
+        auto node = *nodeIt;
+        for (auto operand : node->op->getOperands()) {
+          Operation *childOp = operand.getDefiningOp();
+          if (!lookup.contains(childOp))
+            continue;
+          Node *childNode = lookup.find(childOp)->second;
+          if (currBlock.contains(childNode)) {
+            node->add<Node::ChildType::Real>(childNode);
+          } else {
+            auto *childBlock = getBlock(childNode);
+            assert(childBlock != nullptr);
+            auto *lastNodeInChildBlock = childBlock->back();
+            lastNodeInChildBlock->add<Node::ChildType::Artificials>(childNode);
+          }
+        }
+        if (node->op->getUsers().empty()) {
+          auto *lastNodeInBlock = currBlock.back();
+          if (lastNodeInBlock != node) {
+            lastNodeInBlock->add<Node::ChildType::Artificials>(node);
+          }
+        }
       }
     }
   }
 
   llvm::SmallVector<std::unique_ptr<Node>> nodes;
+  SmallVector<SetVector<Node *>> blocks;
   llvm::MapVector<Operation *, Node *> lookup;
 };
 
@@ -279,174 +114,34 @@ llvm::raw_ostream &operator<<(llvm::raw_ostream &out, Graph &graph) {
   out << "digraph \"dep-graph\" {\n";
   out << "rankdir=\"LR\"\n";
   for (auto [idx, node] : llvm::enumerate(graph.getNodes())) {
-    std::string name = std::to_string(reinterpret_cast<intptr_t>(node));
-    out << name << "\t[label=\"" << node->getOp()->getName() << " ("
-        << node->getWeight() << ") \"]\n";
+    std::string name = std::to_string(reinterpret_cast<intptr_t>(node.get()));
+    out << name << "\t[label=\"" << node->op->getName() << "\"]\n";
   }
   for (auto [idx, node] : llvm::enumerate(graph.getNodes())) {
-    std::string name = std::to_string(reinterpret_cast<intptr_t>(node));
-    for (auto child : node->getRealChildren()) {
+    std::string name = std::to_string(reinterpret_cast<intptr_t>(node.get()));
+    for (auto child : node->realChildren) {
       std::string childName = std::to_string(reinterpret_cast<intptr_t>(child));
       out << "\t" << childName << " -> " << name << ";\n";
     }
-    for (auto child : node->getArtificialChildren()) {
+    for (auto child : node->artificialChildren) {
       std::string childName = std::to_string(reinterpret_cast<intptr_t>(child));
-      out << "\t" << childName << " -> " << name
-          << " [style=\"dashed\", color=\"blue\"];\n";
+      out << "\t" << childName << " -> " << name << " [style=\"dashed\"];\n";
     }
   }
   out << "}";
   return out;
 }
 
-struct GraphManager {
-  GraphManager(Graph &graph) : graph(graph) {
-    for (auto [idx, node] : llvm::enumerate(graph.getNodes())) {
-      nodesIndices.insert({node, idx});
-      if (node->hasNoChildren())
-        leafs.insert(node);
-    }
-  }
-
-  bool finished() { return leafs.empty(); }
-
-  void removeLeaf(Node *node) {
-    assert(node->hasNoChildren());
-    leafs.remove(node);
-    for (auto parent : node->getParents()) {
-      parent->removeChild(node);
-      if (parent->hasNoChildren()) {
-        leafs.insert(parent);
-      }
-    }
-  }
-
-  size_t getNodeSourceCodeIndex(Node *node) {
-    assert(nodesIndices.contains(node));
-    return nodesIndices[node];
-  }
-
-  const SetVector<Node *> &getCurrentLeafs() { return leafs; }
-
-private:
-  Graph graph;
-  SetVector<Node *> leafs;
-  DenseMap<Node *, size_t> nodesIndices;
-};
-
-struct MachineModel {
-  struct Result {
-    Node *selectedNode{nullptr};
-    SmallVector<Node *> normPriorityNodes{};
-    SmallVector<Node *> lowPriorityNodes{};
-    void set(Node *node) {
-      if (!selectedNode) {
-        selectedNode = node;
-      } else {
-        if (node->getWeight() > selectedNode->getWeight()) {
-          selectedNode = node;
-        }
-      }
-    }
-  };
-
-  Result select(const SetVector<Node *> &readyNodes) {
-    Result result;
-    SmallVector<Node *> ldsNodes;
-    SmallVector<Node *> vmemNodes;
-    SmallVector<Node *> mfmaNodes;
-    SmallVector<Node *> barrierNodes;
-
-    for (auto *node : readyNodes) {
-      Operation *op = node->getOp();
-      if (dyn_cast<triton::gpu::LocalLoadOp>(op) ||
-          dyn_cast<triton::gpu::LocalStoreOp>(op) ||
-          dyn_cast<triton::gpu::MemDescSubviewOp>(op)) {
-        ldsNodes.push_back(node);
-        continue;
-      }
-      if (dyn_cast<triton::LoadOp>(op) || dyn_cast<triton::StoreOp>(op) ||
-          dyn_cast<triton::amdgpu::BufferLoadOp>(op) ||
-          dyn_cast<triton::amdgpu::BufferStoreOp>(op)) {
-        vmemNodes.push_back(node);
-        continue;
-      }
-      if (dyn_cast<triton::DotOp>(op)) {
-        mfmaNodes.push_back(node);
-        continue;
-      }
-      if (dyn_cast<mlir::gpu::BarrierOp>(op)) {
-        barrierNodes.push_back(node);
-        continue;
-      }
-      result.normPriorityNodes.push_back(node);
-    }
-
-    if (!vmemNodes.empty()) {
-      if (MachineModel::maxLoadStoreIssues > issuedLoadStoreCounter) {
-        for (auto *node : vmemNodes) {
-          result.set(node);
-        }
-        ++issuedLoadStoreCounter;
-        return result;
-      } else {
-        result.lowPriorityNodes.append(vmemNodes);
-      }
-    }
-
-    if (!ldsNodes.empty()) {
-      if (MachineModel::maxLocalLoadStoreIssues > issuedLocalStoreLoadCounter) {
-        for (auto *node : ldsNodes) {
-          result.set(node);
-        }
-        if (!(dyn_cast<triton::gpu::MemDescSubviewOp>(
-                result.selectedNode->getOp())))
-          ++issuedLocalStoreLoadCounter;
-        return result;
-      } else {
-        result.lowPriorityNodes.append(ldsNodes);
-      }
-    }
-
-    if (!barrierNodes.empty()) {
-      for (auto *node : barrierNodes) {
-        result.set(node);
-      }
-      return result;
-    }
-
-    if (!mfmaNodes.empty()) {
-      issuedLocalStoreLoadCounter =
-          std::max(0, issuedLocalStoreLoadCounter - 1);
-      issuedLoadStoreCounter = std::max(0, issuedLoadStoreCounter - 1);
-      for (auto *node : mfmaNodes) {
-        result.set(node);
-      }
-      return result;
-    }
-
-    return result;
-  }
-
-  void printState(llvm::raw_ostream &stream) {
-    stream << "issuedLoadStoreCounter: " << issuedLoadStoreCounter << "; "
-           << "issuedLocalStoreLoadCounter: " << issuedLocalStoreLoadCounter
-           << '\n';
-  }
-
-private:
-  const inline static int32_t maxLoadStoreIssues{2};
-  const inline static int32_t maxLocalLoadStoreIssues{4};
-  int32_t issuedLoadStoreCounter{0};
-  int32_t issuedLocalStoreLoadCounter{0};
-};
-
 struct TritonAMDGPURescheduleOps
     : public TritonAMDGPURescheduleOpsBase<TritonAMDGPURescheduleOps> {
   explicit TritonAMDGPURescheduleOps(StringRef targetArch) {
     this->arch = targetArch.str();
   }
-  void reschedule(Block *block) {
+
+  void reschedule(Block *mlirBlock) {
+    Graph graph(mlirBlock);
+    llvm::outs() << graph << '\n';
+
     // TODO: build dependency graph
     // TODO: use gpu.barrier as havy-edges
     // TODO: move ops around to improve ILP
