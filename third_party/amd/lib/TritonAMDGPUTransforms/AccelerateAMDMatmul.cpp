cddf0fc3d37f24d05a688bf016f28450a58e650f
Lixun Zhang <lixun.zhang@amd.com>
2025-02-12 04:04:44 +0000

    Force warpsPerCTA={1, numWarps} when BLOCK_M=mDim

--- third_party/amd/lib/TritonAMDGPUTransforms/AccelerateAMDMatmul.cpp
+++ third_party/amd/lib/TritonAMDGPUTransforms/AccelerateAMDMatmul.cpp
@@ -400,8 +400,11 @@ public:
     auto warpsPerTile =
         warpsPerTileMFMA(dotOp, retShape, numWarps, {mDim, nDim});
 
-    // Always use transposed mfma layout. This enables larger vectorization
-    // for global store instructions
+    if (isSecondDot(dotOp) && (retShape[0] == mDim)) {
+      warpsPerTile[0] = 1;
+      warpsPerTile[1] = (unsigned)numWarps;
+    }
+
     mfmaEnc = ttg::AMDMfmaEncodingAttr::get(
         oldRetType.getContext(),
         /*versionMajor*/ mfmaVersion, /*versionMinor*/ 0, warpsPerTile,
