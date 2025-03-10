cdc20f51440b442396101b1da1da9e25a4968927
David Tanner <dtanner@amd.com>
2025-03-10 13:55:28 +0000

    adding fa workspace dependencies

--- python/perf-kernels/fa_workspace/run.sh
+++ python/perf-kernels/fa_workspace/run.sh
@@ -1,11 +1,11 @@
 
 # Run normal.
-#TRITON_ALWAYS_COMPILE=1 MLIR_ENABLE_DUMP=0 TRITON_HIP_STREAM_MAX_DEPTH=1 FA_CONFIG=./config.yaml python3 ./flash-attention.py -b 2 -hq 16 -hk 16 -sq 8192 -sk 8192 -d 128 --dump-ir amdgcn
+TRITON_ALWAYS_COMPILE=1 MLIR_ENABLE_DUMP=0 TRITON_HIP_STREAM_MAX_DEPTH=1 FA_CONFIG=./config.yaml python3 ./flash-attention.py -b 2 -hq 16 -hk 16 -sq 8192 -sk 8192 -d 128 --dump-ir amdgcn
 
 # Dump refined ttgir.
 #TRITON_MLIR_DUMP_REFINE_OPS=1 TRITON_ALWAYS_COMPILE=1 MLIR_ENABLE_DUMP=0 TRITON_HIP_STREAM_MAX_DEPTH=1 FA_CONFIG=./config.yaml python3 ./flash-attention.py -b 2 -hq 16 -hk 16 -sq 8192 -sk 8192 -d 128 --dump-ir amdgcn
 
 # Run custom ttgir.
-TRITON_MLIR_INSERT_REFINE_OPS=ttgir_128x64/6.ttgir TRITON_ALWAYS_COMPILE=1 MLIR_ENABLE_DUMP=0 TRITON_HIP_STREAM_MAX_DEPTH=1 FA_CONFIG=./config.yaml python3 ./flash-attention.py -b 2 -hq 16 -hk 16 -sq 8192 -sk 8192 -d 128 --dump-ir amdgcn
+#TRITON_MLIR_INSERT_REFINE_OPS=ttgir_128x64/6.ttgir TRITON_ALWAYS_COMPILE=1 MLIR_ENABLE_DUMP=0 TRITON_HIP_STREAM_MAX_DEPTH=1 FA_CONFIG=./config.yaml python3 ./flash-attention.py -b 2 -hq 16 -hk 16 -sq 8192 -sk 8192 -d 128 --dump-ir amdgcn
 #TRITON_MLIR_INSERT_REFINE_OPS=ttgir_128x64/4.ttgir TRITON_ALWAYS_COMPILE=1 MLIR_ENABLE_DUMP=0 TRITON_HIP_STREAM_MAX_DEPTH=1 FA_CONFIG=./config.yaml python3 ./flash-attention.py -b 2 -hq 16 -hk 16 -sq 8192 -sk 8192 -d 128 --dump-ir amdgcn
 
