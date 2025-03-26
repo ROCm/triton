9aee61eb2eddcc32e9c67cc53e6e8589a41e730d
David Tanner <dtanner@amd.com>
2025-03-26 15:23:35 +0000

    merged together num_stages=2 and in_thread_transpose

--- python/perf-kernels/run.sh
+++ python/perf-kernels/run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 
-MLIR_ENABLE_DUMP=1 TRITON_ALWAYS_COMPILE=1 TRITON_PRINT_AUTOTUNING=1 python3 ./flash-attention.py -b 2 -hq 16 -hk 16 -sq 8192 -sk 8192 -d 128
+MLIR_ENABLE_DUMP=1 TRITON_ALWAYS_COMPILE=1 TRITON_PRINT_AUTOTUNING=1 python3 ./flash-attention-new.py -b 2 -hq 16 -hk 16 -sq 8192 -sk 8192 -d 128
 
 #python3 ./flash-attention.py -b 16 -hq 16 -hk 16 -sq 8192 -sk 8192 -d 64 -causal -layout bhsd -persistent dynamic
 # TRITON_PRINT_AUTOTUNING=1
