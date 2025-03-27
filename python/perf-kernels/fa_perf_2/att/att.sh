5cd0bf3ff82be12c5da5658b4348cd0eec3a65a3
David Tanner <dtanner@amd.com>
2025-03-27 20:57:00 +0000

    fast fa loop but slow epilogue

--- python/perf-kernels/fa_perf_2/att/att.sh
+++ python/perf-kernels/fa_perf_2/att/att.sh
@@ -3,10 +3,10 @@
 rm -rf triton_cache
 rm -rf ~/.triton/cache
 
-HIP_VISIBLE_DEVICES=3 TRITON_MLIR_INSERT_REFINE_OPS=../ttgir/2.ttgir \
+HIP_VISIBLE_DEVICES=3 TRITON_MLIR_INSERT_REFINE_OPS=../ttgir/7.ttgir \
   TRITON_ALWAYS_COMPILE=1 TRITON_HIP_STREAM_MAX_DEPTH=0 FA_CONFIG=../config.yaml \
   rocprofv2 \
-  -d fa2_2 \
+  -d fa2_7 \
   -i att.txt \
   --plugin att auto \
   --mode file \
