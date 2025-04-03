120928e1f5cca7f416e456acde800225b3c835d0
ravil-mobile <ravil.aviva.com@gmail.com>
2025-04-03 19:19:13 +0000

    [AMD] Fixed MFMA to Linear Layout conversion for 1D tensors

--- lib/Dialect/TritonGPU/IR/LinearLayoutConversions.cpp
+++ lib/Dialect/TritonGPU/IR/LinearLayoutConversions.cpp
@@ -518,7 +518,28 @@ AMDMfmaEncodingAttr::toLinearLayout(ArrayRef<int64_t> shape) const {
         LinearLayout::identity1D(warpsPerCTA[0], kWarp, outDimNames[order[2]]);
   }
 
-  return combineCtaCgaWithShape(tileLayout, getCTALayout(), shape);
+  // And each warp takes the same register and lane sub-layout. So multiply with
+  // an identity layout for the warp.
+  LinearLayout warpLayout =
+      identityStandardND(S("warp"), getWarpsPerCTA(), order);
+  LinearLayout ctaLayout = tileLayout * warpLayout;
+
+  auto combinedLayout =
+      combineCtaCgaWithShape(ctaLayout, getCTALayout(), shape);
+
+  auto bases = combinedLayout.getBases();
+  std::vector<std::vector<int>> newRegBases;
+  for (const auto &basis : bases[S("register")]) {
+    if (llvm::any_of(basis, [](int b) { return b != 0; })) {
+      newRegBases.push_back(basis);
+    }
+  }
+  bases[S("register")] = newRegBases;
+
+  auto result = LinearLayout(std::move(bases),
+                             llvm::to_vector(combinedLayout.getOutDimNames()));
+
+  return result;
 }
 
 LinearLayout chooseDotDsReadB64TrLayout(DotOperandEncodingAttr dotMfmaLayout,
