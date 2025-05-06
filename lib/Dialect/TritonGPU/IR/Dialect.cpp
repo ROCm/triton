ffba9f3c3ca4880ebefd000f5d271224bc79f386
Alexander Weinrauch <alexander.weinrauch@amd.com>
2025-05-06 17:24:48 +0000

    [GEMM] Do not swizzle the scale

--- lib/Dialect/TritonGPU/IR/Dialect.cpp
+++ lib/Dialect/TritonGPU/IR/Dialect.cpp
@@ -1805,6 +1805,12 @@ SwizzledSharedEncodingAttr AMDMfmaEncodingAttr::composeSharedLayoutForOperand(
   if (getMDim() == 4)
     maxPhase = 4;
 
+  // Disable swizzling for scales
+  if (operandIdx >= 2) {
+    return SwizzledSharedEncodingAttr::get(getContext(), 1, 1, 1, sharedOrder,
+                                           ctaLayout);
+  }
+
   return SwizzledSharedEncodingAttr::get(getContext(), vectorSize, perPhase,
                                          maxPhase, sharedOrder, ctaLayout);
 }
