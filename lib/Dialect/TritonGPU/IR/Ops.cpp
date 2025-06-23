966de0693ad0c99e259dc4cdbc884fbfabd1498f
ravil-mobile <ravil.aviva.com@gmail.com>
2025-06-23 14:38:00 +0000

    [AMD] lifted upstream constraints in MemDescSubview::verify

--- lib/Dialect/TritonGPU/IR/Ops.cpp
+++ lib/Dialect/TritonGPU/IR/Ops.cpp
@@ -680,11 +680,14 @@ LogicalResult MemDescSubviewOp::verify() {
   //  - We split along at most one dim, but just with constant values
   //  - The values where the split happens must not be within the swizzling
   //  pattern
-  // Check which dimensions we are splitting along
-  SetVector<int> splitDims{};
+  // Check which dimension we are splitting along
+  // int dim = -1;
+
+  // TODO: discuss with upstream folks
+  SetVector<int> slicedDims{};
   for (int i = 0; i < srcTy.getRank(); i++) {
     if (srcTy.getDimSize(i) != dstTy.getDimSize(i)) {
-      splitDims.insert(i);
+      slicedDims.insert(i);
     }
   }
   SmallVector<int64_t> offsets;
@@ -695,12 +698,12 @@ LogicalResult MemDescSubviewOp::verify() {
     offsets.push_back(value.getSExtValue());
   }
   // Identity subview
-  if (splitDims.empty()) {
+  if (slicedDims.empty()) {
     return success();
   }
 
   for (auto [dim, offset] : llvm::enumerate(offsets)) {
-    if (!splitDims.contains(dim)) {
+    if (!slicedDims.contains(dim)) {
       if (offset != 0) {
         return emitError("A non zero offset found in a dimension that is "
                          "not being split");
@@ -715,15 +718,10 @@ LogicalResult MemDescSubviewOp::verify() {
   auto ctx = getContext();
   // The order gives us the honest-to-goodness layout rank
   auto srcAllocShape = srcTy.getAllocShape().take_back(getOrder(srcTy).size());
-  auto ll = triton::gpu::toLinearLayout(srcAllocShape, srcTy.getEncoding());
-  // NYI: We don't support non-trivial block dimension for now.
-  auto kBlock = mlir::StringAttr::get(getContext(), "block");
-  if (ll.getInDimSize(kBlock) != 1) {
-    return emitError("non-trivial block dimension not supported");
-  }
+  auto llInv =
+      triton::gpu::toLinearLayout(srcAllocShape, srcTy.getEncoding()).invert();
 
-  auto llInv = ll.invert();
-  for (auto dim : splitDims) {
+  for (auto dim : slicedDims) {
     auto kDim = mlir::StringAttr::get(ctx, "dim" + llvm::Twine(dim));
     llvm::SmallVector<std::pair<mlir::StringAttr, int32_t>> namedOffsets;
     for (auto d : standardOutDimNames(ctx, srcTy.getRank())) {
