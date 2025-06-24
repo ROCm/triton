164487a6abd4fa2b945d36a1a046763f3d79f954
ravil-mobile <ravil.aviva.com@gmail.com>
2025-06-24 14:37:39 +0000

    [AMD] implemented downcasting from bf16 to Fp8E4M3FN

--- third_party/amd/lib/TritonAMDGPUToLLVM/ElementwiseOpToLLVM.cpp
+++ third_party/amd/lib/TritonAMDGPUToLLVM/ElementwiseOpToLLVM.cpp
@@ -935,13 +935,89 @@ static ConverterT Bf16_to_Fp8E5M2(AMD::ISAFamily isaFamily) {
   return isaFamily == AMD::ISAFamily::CDNA4 ? Bf16_to_Fp8E5M2_HW
                                             : Bf16_to_Fp8E5M2_SW;
 }
-// Bf16 -> OCP Fp8
-static SmallVector<Value> Bf16_to_Fp8E4M3FN(Location loc,
-                                            ConversionPatternRewriter &rewriter,
-                                            const SmallVector<Value> &v) {
+
+// Converts to both Fp8E4M3FN and Fp8E4M3FNUZ from Bf16
+// FN - only nan values and no infinite values;
+// UZ - no negative zero
+// see: https://onnx.ai/onnx/technical/float8.html
+template <bool UZ>
+static SmallVector<Value>
+Bf16_to_Fp8E4M3FN_SW(Location loc, ConversionPatternRewriter &rewriter,
+                     const SmallVector<Value> &v) {
+  assert(v.size() == 2);
+
+  auto b = TritonLLVMOpBuilder(loc, rewriter);
+  auto convert = [&](Value v) {
+    auto i16 = b.bitcast(v, i16_ty);
+    Value sign = b.lshr(b.and_(i16_ty, i16, b.i16_val(0x8000)), b.i16_val(8));
+    Value exp = b.lshr(b.and_(i16_ty, i16, b.i16_val(0x7F80)), b.i16_val(7));
+    Value mantissa = b.and_(i16_ty, i16, b.i16_val(0x007F));
+
+    Value e = b.i16_val(0x0000);
+    Value m = b.i16_val(0x0000);
+
+    Value newExp = b.sub(exp, b.i16_val(120));
+
+    e = b.and_(newExp, b.i16_val(0x000F));
+    m = b.and_(b.lshr(mantissa, b.i16_val(7 - 3)), b.i16_val(0x0007));
+
+    // handle special cases
+    Value isBf16ExprZero = b.icmp_eq(exp, b.i16_val(0x0000));
+    Value isBf16MantissaZero = b.icmp_eq(mantissa, b.i16_val(0x0000));
+    Value isBf16Null = b.and_(isBf16ExprZero, isBf16MantissaZero);
+    if constexpr (UZ) {
+      sign = b.select(isBf16Null, b.i16_val(0x0000), sign);
+    }
+
+    Value isBf16ExpFull = b.icmp_eq(exp, b.i16_val(0x00FF));
+    Value isBf16Nan =
+        b.and_(isBf16ExpFull, b.icmp_ne(mantissa, b.i16_val(0x0000)));
+    e = b.select(isBf16Nan, b.i16_val(0x000F), e);
+    m = b.select(isBf16Nan, b.i16_val(0x0007), m);
+    if constexpr (UZ) {
+      sign = b.select(isBf16Nan, b.i16_val(0x0000), sign);
+    }
+
+    Value isBf16Inf = b.and_(isBf16ExpFull, isBf16MantissaZero);
+    e = b.select(isBf16Inf, b.i16_val(0x000F), e);
+    if constexpr (UZ) {
+      m = b.select(isBf16Inf, b.i16_val(0x0007), m);
+      sign = b.select(isBf16Inf, b.i16_val(0x0000), sign);
+    } else {
+      m = b.select(isBf16Inf, b.i16_val(0x0006), m);
+    }
+
+    Value isGreaterFP8Max = b.icmp_sge(exp, b.i16_val(0x000F));
+    isGreaterFP8Max =
+        b.and_(isGreaterFP8Max, b.icmp_sge(mantissa, b.i16_val(0x0060)));
+
+    e = b.select(isGreaterFP8Max, b.i16_val(0x000F), e);
+    m = b.select(isGreaterFP8Max, b.i16_val(0x0006), m);
+
+    Value result = b.or_(b.or_(sign, b.shl(e, b.i16_val(3))), m);
+    auto fp8x2VecTy = vec_ty(i8_ty, 2);
+    result = b.bitcast(result, fp8x2VecTy);
+    return b.extract_element(i8_ty, result, b.i32_val(0));
+  };
+
+  SmallVector<Value> results{convert(v[0]), convert(v[1])};
+  return results;
+}
+
+static SmallVector<Value>
+Bf16_to_Fp8E4M3FN_HW(Location loc, ConversionPatternRewriter &rewriter,
+                     const SmallVector<Value> &v) {
   assert(v.size() == 2);
   return cvtScalePkDowncastToFp8<ROCDL::CvtScaleF32PkFp8Bf16Op>(loc, rewriter,
                                                                 v[0], v[1]);
+}
+
+// Bf16 -> OCP Fp8
+static ConverterT Bf16_to_Fp8E4M3FN(AMD::ISAFamily isaFamily) {
+  constexpr bool enableUZMode = false;
+  return isaFamily == AMD::ISAFamily::CDNA4
+             ? Bf16_to_Fp8E4M3FN_HW
+             : Bf16_to_Fp8E4M3FN_SW<enableUZMode>;
 }
 
 // fp8e4m3fn to bf16
@@ -1006,12 +1082,19 @@ Fp8E4M3FNUZ_to_Bf16(Location loc, ConversionPatternRewriter &rewriter,
 
 // bf16 to fp8e4m3fnuz
 static SmallVector<Value>
-Bf16_to_Fp8E4M3FNUZ(Location loc, ConversionPatternRewriter &rewriter,
-                    const SmallVector<Value> &v) {
+Bf16_to_Fp8E4M3FNUZ_HW(Location loc, ConversionPatternRewriter &rewriter,
+                       const SmallVector<Value> &v) {
   assert(v.size() == 2);
   auto v0 = convertBf16ToFp32(loc, rewriter, v[0]);
   auto v1 = convertBf16ToFp32(loc, rewriter, v[1]);
   return cvtPkFp32ToF8<ROCDL::CvtPkFp8F32Op>(loc, rewriter, v0, v1);
+}
+
+static ConverterT Bf16_to_Fp8E4M3FNUZ(AMD::ISAFamily isaFamily) {
+  constexpr bool enableUZMode = true;
+  return isaFamily == AMD::ISAFamily::CDNA4
+             ? Bf16_to_Fp8E4M3FNUZ_HW
+             : Bf16_to_Fp8E4M3FN_SW<enableUZMode>;
 }
 
 // fp8e5m2fnuz to bf16
@@ -1239,11 +1322,12 @@ struct FpToFpOpConversion
             // BF16 -> F8
             {{BF16TyID, F8E5M2TyID, RoundingMode::RTNE},
              Bf16_to_Fp8E5M2(isaFamily)},
-            {{BF16TyID, F8E4M3FNTyID, RoundingMode::RTNE}, Bf16_to_Fp8E4M3FN},
+            {{BF16TyID, F8E4M3FNTyID, RoundingMode::RTNE},
+             Bf16_to_Fp8E4M3FN(isaFamily)},
             {{BF16TyID, F8E5M2FNUZTyID, RoundingMode::RTNE},
              Bf16_to_Fp8E5M2FNUZ},
             {{BF16TyID, F8E4M3FNUZTyID, RoundingMode::RTNE},
-             Bf16_to_Fp8E4M3FNUZ},
+             Bf16_to_Fp8E4M3FNUZ(isaFamily)},
             // F32 <-> F8
             {{F32TyID, F8E4M3FNUZTyID, RoundingMode::RTNE},
              Fp32_to_Fp8E4M3FNUZ},
