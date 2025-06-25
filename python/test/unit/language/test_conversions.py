5d2fe7cb379bce539bf74d02d4ab4bf2a9b7f774
PMylon <pmylonas@amd.com>
2025-06-25 14:31:54 +0000

    [Temp] Comment out skip logic in order to test on CDNA4 arch

--- python/test/unit/language/test_conversions.py
+++ python/test/unit/language/test_conversions.py
@@ -350,8 +350,8 @@ def test_typeconvert_downcast(src_dtype, dst_dtype, rounding, max_repr, device):
             if not (is_hip_cdna3() and src_dtype == 'float16' or is_hip_cdna4()):
                 pytest.skip("float8e4nv downcast tests only supported on AMDGPU CDNA3 or on CDNA4 and from float16 with RTNE rounding")
 
-        if dst_dtype in ('float8e5b16', 'float8e4b8') and rounding == 'rtne' and not is_hip_cdna3():
-            pytest.skip(f"{dst_dtype} downcast with RTNE rounding tests only supported on AMDGPU CDNA3")
+        #if dst_dtype in ('float8e5b16', 'float8e4b8') and rounding == 'rtne' and not is_hip_cdna3():
+            #pytest.skip(f"{dst_dtype} downcast with RTNE rounding tests only supported on AMDGPU CDNA3")
 
     # dtype : (exponent_bits, mantissa_bits, exponent_bias)
     stuff = {
