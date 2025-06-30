ae90523c6d49767f2fdbf4786b3c5194967243da
Xinya Zhang <Xinya.Zhang@amd.com>
2025-06-30 18:00:53 -0500

    Add bit_length to constexpr.

--- python/triton/language/core.py
+++ python/triton/language/core.py
@@ -333,6 +333,9 @@ class constexpr(base_value):
         args = (_unwrap_if_constexpr(x) for x in _normalize_tuple(args))
         return self.value.__getitem__(*args)
 
+    def bit_length(self):
+        return constexpr(self.value.bit_length())
+
 
 def constexpr_function(f):
     """
