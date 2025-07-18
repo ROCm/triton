3e140d9e24dfab0569d1309077fb922d56b1e098
Xiaohu Guo <Xiaohu.Guo@amd.com>
2025-07-18 10:09:12 -0500

    fix incorrect num_sms

--- python/perf-kernels/streamk/utils/utils.py
+++ python/perf-kernels/streamk/utils/utils.py
@@ -30,6 +30,12 @@ name_to_tl_types = {
     'bf8': tl.float8e5b16,
 }
 
+def get_num_sms():
+    # Returns the Compute Unit count of the current device
+    current_device_index = torch.cuda.current_device()
+    current_device = torch.cuda.get_device_properties(current_device_index)
+    num_sms = current_device.multi_processor_count
+    return num_sms
 
 def run_bash_command_wrapper(commandstring, capture=True):
     try:
