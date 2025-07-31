16bb2dc2f68ce93b161fe6ef6a0612ce2de552e0
Lixun Zhang <lixun.zhang@amd.com>
2025-07-31 05:00:47 +0000

    Force swizzledSharedLayout for all users of load
    In hstu bwd kernel, the load has two users
    1. regular cvt from #blocked to #dotOp
    2. special cvt from #blocked to #linear
    
    The second cvt is doing the actually transpose operation so that the
    following transOp is a no-op.
    Currently we only expect #linear for scaled fp8 operands and a special
    LDS layout is chosen.
    This commit simply forces every user to use the swizzled LDS layout to
    enable the loads to be pipelined

--- third_party/amd/lib/TritonAMDGPUTransforms/StreamPipeline.cpp
+++ third_party/amd/lib/TritonAMDGPUTransforms/StreamPipeline.cpp
@@ -256,6 +256,8 @@ getSharedEncIfAllUsersAreDotEnc(Value loadedValue) {
         tempAttr = ttg::SwizzledSharedEncodingAttr::get(
             loadedValue.getContext(), dotOpEnc, srcTy.getShape(), sharedOrder,
             ctaLayout, bitWidth, /*needTrans=*/false);
+        attr = tempAttr;
+        break;
       } else if (auto llEnc = dyn_cast<ttg::LinearEncodingAttr>(userResEnc)) {
         // We use linear layout directly for scaled dot fp8 operands. For such
         // cases, we need to look further down the def-use chain to find the dot
