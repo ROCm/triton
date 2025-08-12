ba459d1607be6c528df88b8effbe0f1bb53306ad
Mika Laitio <mika.laitio@amd.com>
2025-08-11 20:53:18 -0700

    Fix PR 7780 merge error on release/internal/3.3.x branch (#858)
    Fixes the merge error of PR 7780 when it was cherry-picked to ROCm release/internal/3.3.x branch.
    Problem was detected when building the Pytorch from ROCm release/2.7 branch which uses
    the Triton from ROCm release/internal/3.3.x branch.
    
    There are two changes:
    - remove the merge conflict lines
    - renames the _semantic from original patch to _builder as used in the 3.3.x branch
    
    fix: https://github.com/ROCm/triton/issues/857
    
    Signed-off-by: Mika Laitio <mika.laitio@amd.com>

--- third_party/amd/language/hip/libdevice.py
+++ third_party/amd/language/hip/libdevice.py
@@ -74,18 +74,14 @@ def fast_expf(arg0, _builder=None):
 
 
 @core.extern
-<<<<<<< HEAD
-def fast_dividef(arg0, arg1, _builder=None):
-=======
-def fast_tanhf(arg0, _semantic=None):
+def fast_tanhf(arg0, _builder=None):
     return core.extern_elementwise("", "", [arg0], {
         (core.dtype("fp32"), ): ("__triton_hip_fast_tanhf", core.dtype("fp32")),
-    }, is_pure=True, _semantic=_semantic)
+    }, is_pure=True, _builder=_builder)
 
 
 @core.extern
-def fast_dividef(arg0, arg1, _semantic=None):
->>>>>>> 353540798c ([AMD] Add fast_tanhf to libdevice (#7780))
+def fast_dividef(arg0, arg1, _builder=None):
     return core.extern_elementwise("", "", [arg0, arg1], {
         (core.dtype("fp32"), core.dtype("fp32")): ("__triton_hip_fast_fdividef", core.dtype("fp32")),
     }, is_pure=True, _builder=_builder)
