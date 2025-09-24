49d43a902e6a76e69c8b00b032129a3258f92005
Tanner, David <David.Tanner@amd.com>
2025-09-24 10:14:04 -0400

    Re-enabling mxfa ci tests. These tests were already passing locally; now that CI has been updated they should pass on CI too. Also, using NUM_THREADS=1 since multi-threading didn't make it noticeably faster and may have contributed to instability. (#147)
    Co-authored-by: David Tanner <dtanner@amd.com>

--- .github/workflows/gfx1250.sh
+++ .github/workflows/gfx1250.sh
@@ -58,6 +58,4 @@ pytest --count=16 -n 16 -s -v mi400/test_mxgemm_hipdriver.py
 
 echo "=== Run Attention Tests ==="
 
-let "NUM_PROC = $(nproc) / 2"
-export HSA_MODEL_NUM_THREADS=${NUM_PROC}
-# pytest -v -s mi400/test_mxfa_hipdriver.py # TODO enable
+pytest --count=16 -n 16 -v -s mi400/test_mxfa_hipdriver.py
