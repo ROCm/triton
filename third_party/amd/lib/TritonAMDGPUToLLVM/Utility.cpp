113bdc90825fccc38b1aaae5eb8890bb3f830949
Dewei Wang <Dewei.Wang@amd.com>
2025-09-24 09:57:44 +0800

    port fp4->bf16

--- third_party/amd/lib/TritonAMDGPUToLLVM/Utility.cpp
+++ third_party/amd/lib/TritonAMDGPUToLLVM/Utility.cpp
@@ -684,8 +684,9 @@ bool isChainDotTail(tt::DotOpInterface dotOp) {
   return false;
 }
 
-SmallVector<Value, 4> upcast8xMxfp4_SW(RewriterBase &rewriter, Operation *op,
-                                       bool toFp16, Value packedVec, Value scale) {
+SmallVector<Value> upcast8xMxfp4_SW(RewriterBase &rewriter, Operation *op,
+                                    bool toFp16, Value packedVec,
+                                    ISAFamily isaFamily, Value scale) {
   assert((isa<triton::amdgpu::UpcastMXFPOp, triton::gpu::Fp4ToFpOp>(op)) &&
          "Expected UpcastMXFPOp or Fp4ToFpOp");
   Location loc = op->getLoc();
@@ -700,7 +701,7 @@ SmallVector<Value, 4> upcast8xMxfp4_SW(RewriterBase &rewriter, Operation *op,
   Value input = b.bitcast(packedVec, i32_ty);
 
   // fp4 to bf16 for cdna3: fp4->fp8->fp32
-  if (!toFp16) {
+  if (isaFamily == ISAFamily::CDNA3 && !toFp16) {
     // Step 1: extract EM bits for elements 0,2,4,6 and 1,3,5,7 respectively.
     // e2m1_6420_idx = | 0[0e6EM] | 0[0e4EM] | 0[0e2EM] | 0[0e0EM] |
     Value e2m1_6420_idx = b.and_(input, b.i32_val(0x07070707));
@@ -758,12 +759,20 @@ SmallVector<Value, 4> upcast8xMxfp4_SW(RewriterBase &rewriter, Operation *op,
     Value e5 = b.extract_element(pkVals[3], b.i32_val(0));
     Value e6 = b.extract_element(pkVals[1], b.i32_val(1));
     Value e7 = b.extract_element(pkVals[3], b.i32_val(1));
+    SmallVector<Value, 8> f32Vals{e0, e1, e2, e3, e4, e5, e6, e7};
     Value sel = b.i32_val(0x07060302);
-    Value res_10 = LLVM::createLLVMCallOp(rewriter, loc, funcOp, {e1, e0, sel}).getResult();
-    Value res_32 = LLVM::createLLVMCallOp(rewriter, loc, funcOp, {e3, e2, sel}).getResult();
-    Value res_54 = LLVM::createLLVMCallOp(rewriter, loc, funcOp, {e5, e4, sel}).getResult();
-    Value res_76 = LLVM::createLLVMCallOp(rewriter, loc, funcOp, {e7, e6, sel}).getResult();
-    return {res_10, res_32, res_54, res_76};
+    SmallVector<Value> results;
+    for (unsigned i = 0; i < 8; i += 2) {
+      // v2f32->v2bf16: {e1.f32[31:16], e0.f32[31:16]}
+      Value res = LLVM::createLLVMCallOp(rewriter, loc, funcOp,
+                                         {f32Vals[i + 1], f32Vals[i], sel})
+                      .getResult();
+      Type v2bf16 = vec_ty(bf16_ty, 2);
+      res = b.bitcast(res, v2bf16);
+      results.push_back(b.extract_element(res, b.i32_val(0)));
+      results.push_back(b.extract_element(res, b.i32_val(1)));
+    }
+    return results;
   }
 
   // MXFP4 has 4 bits, S.EE.M, for Sign, Exponent, and Mantissa respectively.
@@ -890,7 +899,15 @@ SmallVector<Value, 4> upcast8xMxfp4_SW(RewriterBase &rewriter, Operation *op,
                                         {res_75, res_64, b.i32_val(0x07060302)})
                      .getResult();
 
-  return {res_10, res_32, res_54, res_76};
+  SmallVector<Value, 4> pkVals{res_10, res_32, res_54, res_76};
+  SmallVector<Value> results;
+  Type elmTy = toFp16 ? f16_ty : bf16_ty;
+  for (int j = 0; j < 4; j++) {
+    Value elements = b.bitcast(pkVals[j], vec_ty(elmTy, 2));
+    results.push_back(b.extract_element(elements, b.i32_val(0)));
+    results.push_back(b.extract_element(elements, b.i32_val(1)));
+  }
+  return results;
 }
 
 } // namespace mlir::LLVM::AMD
