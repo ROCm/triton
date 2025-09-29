fa1a5fa3096ae5ead282f46fa3ff0552bccd70f3
Lixun Zhang <lixun.zhang@amd.com>
2025-09-29 16:42:59 +0000

    Extand buffer_load to have index and offset

--- python/src/gluon_ir.cc
+++ python/src/gluon_ir.cc
@@ -738,9 +738,9 @@ void init_gluon_ir(py::module &&m) {
       .def("create_buffer_load",
            [](GluonOpBuilder &self, Type resultType, Value ptr, Value offsets,
               Value mask, Value other, tt::CacheModifier cache) -> Value {
-             return self.create<ttag::BufferLoadOp>(resultType, ptr, offsets,
-                                                    Value() /*stride*/, cache,
-                                                    mask, other);
+             return self.create<ttag::BufferLoadOp>(
+                 resultType, ptr, Value() /*index*/, offsets,
+                 Value() /*stride*/, cache, mask, other);
            })
       .def("create_buffer_store",
            [](GluonOpBuilder &self, Value storedValue, Value ptr, Value offsets,
