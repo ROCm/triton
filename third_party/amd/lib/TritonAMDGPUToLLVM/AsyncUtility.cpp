85c595d4d1f56fceb627be972620e1c9a93adf4e
Lei Zhang <antiagainst@gmail.com>
2025-10-07 05:41:32 +0000

    Revert "[AMD] Use lowerLdSt for local_load to ds_read_tr path (#8344)"
    This reverts commit 5201154b6dfd83373439e6802e3ef5a7acdc358b.

--- third_party/amd/lib/TritonAMDGPUToLLVM/AsyncUtility.cpp
+++ third_party/amd/lib/TritonAMDGPUToLLVM/AsyncUtility.cpp
@@ -3,7 +3,6 @@
 #include "Dialect/TritonAMDGPU/IR/Dialect.h"
 #include "TargetInfo.h"
 #include "triton/Dialect/TritonGPU/IR/Dialect.h"
-#include "llvm/ADT/TypeSwitch.h"
 
 namespace mlir::triton::AMD {
 namespace {
@@ -51,20 +50,21 @@ bool comesFromAsyncWait(Value token) {
 } // namespace
 
 void annotateLocalLoadsSyncedViaAsyncWait(ModuleOp mod) {
-  auto *ctx = mod->getContext();
-
-  mod->walk([&](Operation *op) {
-    TypeSwitch<Operation *, void>(op)
-        .Case<triton::gpu::LocalLoadOp,
-              triton::amdgpu::LocalLoadPackedTransposedOp>([&](auto loadOp) {
-          if (loadOp->hasAttr(syncedViaAsyncWaitAttrName))
-            return;
-          Value token = loadOp.getToken();
-          bool isSyncedViaAsyncWait = token && comesFromAsyncWait(token);
-          loadOp->setAttr(syncedViaAsyncWaitAttrName,
-                          BoolAttr::get(ctx, isSyncedViaAsyncWait));
-        });
+  SmallVector<triton::gpu::LocalLoadOp> localLoads;
+  mod->walk([&](triton::gpu::LocalLoadOp localLoadOp) {
+    localLoads.emplace_back(localLoadOp);
   });
+
+  auto *ctx = mod->getContext();
+  for (auto &loadOp : localLoads) {
+    auto token = loadOp.getToken();
+    if (loadOp->hasAttr(syncedViaAsyncWaitAttrName))
+      continue;
+
+    bool isSyncedViaAsyncWait = token && comesFromAsyncWait(token);
+    loadOp->setAttr(syncedViaAsyncWaitAttrName,
+                    BoolAttr::get(ctx, isSyncedViaAsyncWait));
+  }
 }
 
 bool isSyncedViaAsyncWait(Operation *op) {
@@ -112,10 +112,8 @@ void addAsyncCopyAliasScope(LLVM::AliasAnalysisOpInterface directToLdsOp) {
   directToLdsOp.setAliasScopes(b.getArrayAttr(getAsyncCopyScope(ctx)));
 }
 
-void addLocalLoadNoAliasScope(Operation *localLoadOp,
+void addLocalLoadNoAliasScope(triton::gpu::LocalLoadOp localLoadOp,
                               LLVM::AliasAnalysisOpInterface llLoadOp) {
-  if (!localLoadOp->hasTrait<OpTrait::LocalLoadTrait>())
-    return;
   if (!isSyncedViaAsyncWait(localLoadOp))
     return;
 
