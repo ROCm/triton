9b9b41a50602e06ee9f22a94f5573b9cdbd8b534
Zhao, Pengzhan <Pengzhan.Zhao@amd.com>
2025-10-24 20:22:01 -0700

    Expose get wmma/mfma scale layout (#224)
    * [AMD][GLUON] Expose get wmma/mfma scale layout (#8496)
    
    This PR exposes the internal layout utility
    `chooseScaledMfmaScaleLayout` and `chooseScaledWmmaScaleLayout` for
    Gluon, to help generate a linear layout for scale used in
    `mfma_scaled`/`wmma_scaled`. This also allows gluon kernels to specify a
    scalar scale value or leave it as None.
    
    ---------
    
    Co-authored-by: Pengzhan Zhao <borontion@gmail.com>

--- python/src/gluon_ir.cc
+++ python/src/gluon_ir.cc
@@ -373,6 +373,16 @@ void init_gluon_ir(py::module &&m) {
                  ctx, version, warpsPerCta, instrShape, transposed, ctaLayout,
                  tilesPerWarp, elementBitWidth);
            })
+      .def("get_amd_mfma_scale_layout",
+           [](GluonOpBuilder &self, unsigned opIdx, std::vector<int64_t> &shape,
+              unsigned mfmaMDim, std::vector<unsigned> &tilesPerWarp,
+              std::vector<unsigned> &warpsPerCTA) -> py::object {
+             auto ctx = self.getContext();
+             auto ll = ttg::chooseScaledMfmaScaleLayout(
+                 ctx, opIdx, shape, mfmaMDim, tilesPerWarp, warpsPerCTA);
+             auto attr = ttg::LinearEncodingAttr::get(ctx, ll);
+             return layoutToGluon(attr);
+           })
       .def("get_amd_wmma_layout",
            [](GluonOpBuilder &self, unsigned version, bool transposed,
               std::vector<unsigned> &warpsPerCta,
@@ -387,6 +397,16 @@ void init_gluon_ir(py::module &&m) {
              return ttg::AMDWmmaEncodingAttr::get(ctx, version, transposed,
                                                   warpsPerCta, tilesPerWarp,
                                                   ctaLayout, instrShape);
+           })
+      .def("get_amd_wmma_scale_layout",
+           [](GluonOpBuilder &self, unsigned opIdx, std::vector<int64_t> &shape,
+              unsigned mfmaMDim, std::vector<unsigned> &tilesPerWarp,
+              std::vector<unsigned> &warpsPerCTA) -> py::object {
+             auto ctx = self.getContext();
+             auto ll = ttg::chooseScaledWmmaScaleLayout(
+                 ctx, opIdx, shape, mfmaMDim, tilesPerWarp, warpsPerCTA);
+             auto attr = ttg::LinearEncodingAttr::get(ctx, ll);
+             return layoutToGluon(attr);
            })
       .def("get_padded_shared_layout",
            [](GluonOpBuilder &self, std::vector<unsigned> &intervals,
