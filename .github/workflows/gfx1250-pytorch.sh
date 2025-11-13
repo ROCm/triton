9c4116ab5583a90e0f6e21da52260ca9eb8fe15a
Aananthakrishnan, Sriram <Sriram.Aananthakrishnan@amd.com>
2025-11-12 23:50:33 -0500

    [CI] Add a CI step for triton unit tests (#291)

---
+++ .github/workflows/gfx1250-pytorch.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# This script is meant to be used by the CI bots so paths in the below
+# are quite specific to the docker and file organizations within.
+#
+# Don't run it as a generally applicable script!
+set -xeo pipefail
+
+pwd && ls
+
+echo "=== Clean up cache ==="
+
+rm -rf ~/.triton/cache
+
+echo "=== Setup Environment ==="
+
+cd /ffm && source ffmlite_env.sh && cd -
+export HSA_MODEL_NUM_THREADS=1
+export HSA_MODEL_TOML=".github/workflows/ffm_config.toml"
+export HSA_MODEL_ARGS=ffm_enable_time_slicing
+# Add ROCm libs in front of ffm
+export LD_LIBRARY_PATH=/opt/rocm/lib:$LD_LIBRARY_PATH
+
+echo "=== Build and Install Triton ==="
+
+export PYTHON="python3"
+export TRITON_BUILD_WITH_CLANG_LLD="TRUE"
+export TRITON_BUILD_WITH_CCACHE="TRUE"
+export CCACHE_COMPRESS="true"
+
+LLVM_LIBRARY_DIR=/llvm LLVM_SYSPATH=/llvm pip3 install --no-build-isolation .
+
+echo "=== Sanity Check ==="
+
+python3 -c "import triton; print(triton.runtime.driver.active.get_current_target())"
+
+export TRITON_HIP_USE_ASYNC_COPY=1
+
+echo "=== Run Triton Unit Tests ==="
+
+pytest --count=1 -n 12 python/test/unit/language/test_core.py::test_scan2d
