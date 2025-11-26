a3e2d69e07e50d5b36bf8f8e70ab80d5bd53ca62
Jason Furmanek <Jason.Furmanek@amd.com>
2025-11-26 00:30:25 +0000

    hard code location

--- python/setup.py
+++ python/setup.py
@@ -234,7 +234,7 @@ def get_llvm_package_info():
     # Create a stable symlink that doesn't include revision
     sym_name = f"llvm-{system_suffix}"
     #url = f"https://oaitriton.blob.core.windows.net/public/llvm-builds/{name}.tar.gz"
-    url = f"https://github.com/llvm/llvm-project/archive/{name}.zip"
+    url = f"https://github.com/llvm/llvm-project/archive/6e3b47597fabb8df8cf822331461cecbac907c6f.zip"
     return Package("llvm", name, url, "LLVM_INCLUDE_DIRS", "LLVM_LIBRARY_DIR", "LLVM_SYSPATH", sym_name=sym_name)
 
 
