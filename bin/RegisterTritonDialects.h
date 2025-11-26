67a67a815b62de1308a14178b4175f9c1e97475c
Park, Jungwook <Jungwook.Park@amd.com>
2025-11-26 19:11:30 +0000

    [AMD] Add support for the warp-pipeline (#349)

--- bin/RegisterTritonDialects.h
+++ bin/RegisterTritonDialects.h
@@ -98,6 +98,7 @@ inline void registerTritonDialects(mlir::DialectRegistry &registry) {
   mlir::triton::registerConvertTritonAMDGPUToLLVM();
   mlir::triton::registerConvertBuiltinFuncToLLVM();
   mlir::triton::registerOptimizeAMDLDSUsage();
+  mlir::triton::registerConvertWarpPipeline();
 
   mlir::ub::registerConvertUBToLLVMInterface(registry);
   mlir::registerConvertNVVMToLLVMInterface(registry);
@@ -121,6 +122,7 @@ inline void registerTritonDialects(mlir::DialectRegistry &registry) {
   mlir::registerTritonAMDGPUUpdateAsyncWaitCount();
   // mlir::registerTritonAMDGPUPlanCTAPass();
   mlir::registerTritonAMDGPUCoalesceAsyncCopyPass();
+  mlir::registerTritonAMDGPUWarpPipeline();
   mlir::triton::registerTritonAMDGPUInsertInstructionSchedHints();
   mlir::triton::registerTritonAMDGPULowerInstructionSchedHints();
   mlir::registerTritonAMDFoldTrueCmpI();
