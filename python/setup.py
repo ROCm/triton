b2cfcca298c7001dde4fe4fc0a83e1910386083b
Jason Furmanek <Jason.Furmanek@amd.com>
2025-11-27 05:45:19 +0000

    Add S3 location for updated LLVM tar

--- python/setup.py
+++ python/setup.py
@@ -233,8 +233,7 @@ def get_llvm_package_info():
     name = f"llvm-{rev}-{system_suffix}"
     # Create a stable symlink that doesn't include revision
     sym_name = f"llvm-{system_suffix}"
-    #url = f"https://oaitriton.blob.core.windows.net/public/llvm-builds/{name}.tar.gz"
-    url = f"file:///root/llvm-6e3b4759-almalinux-x64.tar.gz"
+    url = f"https://rocm-ci-qa-artifacts.s3.us-east-2.amazonaws.com/llvm-trition%2Fllvm-6e3b4759-almalinux-x64.tar.gz"
     return Package("llvm", name, url, "LLVM_INCLUDE_DIRS", "LLVM_LIBRARY_DIR", "LLVM_SYSPATH", sym_name=sym_name)
 
 
