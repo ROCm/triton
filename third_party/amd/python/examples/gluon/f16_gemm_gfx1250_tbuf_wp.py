bc128b81d221041229661b2c100e2c07c374813d
Park, Jungwook <Jungwook.Park@amd.com>
2026-01-07 20:14:10 +0000

    Fix warp-pipelined gemm kernel (#430)
    - Fix waitcnt : previous cnt was wrong, hidden by pipelined execution. Now verified with much longer memory latency.
    - Fix wmma_layout interface
    - Print out result tensor for the sanity check.

--- third_party/amd/python/examples/gluon/f16_gemm_gfx1250_tbuf_wp.py
+++ third_party/amd/python/examples/gluon/f16_gemm_gfx1250_tbuf_wp.py
@@ -190,14 +190,15 @@ def gemm_tdm_pipelined_warp_pipelined_kernel(a_ptr, b_ptr, c_ptr,  #
                                              BLOCK_K: ttgl.constexpr,  #
                                              NUM_BUFFERS: ttgl.constexpr,  #
                                              TRANSPOSE_B: ttgl.constexpr,  #
-                                             NUM_WARPS: ttgl.constexpr):
+                                             WARP_BASES: ttgl.constexpr):
     a_dtype: ttgl.constexpr = a_ptr.type.element_ty
     b_dtype: ttgl.constexpr = b_ptr.type.element_ty
     ttgl.static_assert(a_dtype.is_fp16() or a_dtype.is_bf16(), "Only fp16/bf16 supported for A")
     ttgl.static_assert(b_dtype.is_fp16() or b_dtype.is_bf16(), "Only fp16/bf16 supported for B")
     ttgl.static_assert(NUM_BUFFERS >= 2, "NUM_BUFFERS must be at least 2")
 
-    WMMA_LAYOUT: ttgl.constexpr = ttgl.amd.AMDWMMALayout(3, True, [NUM_WARPS // 2, 2], [16, 16, 32])
+    WMMA_LAYOUT: ttgl.constexpr = ttgl.amd.AMDWMMALayout(3, True, WARP_BASES, [], [16, 16, 32])
+
     shared_layouts: ttgl.constexpr = create_shared_layouts(BLOCK_M, BLOCK_N, BLOCK_K, TRANSPOSE_B)
     SHARED_LAYOUT_A: ttgl.constexpr = shared_layouts[0]
     SHARED_LAYOUT_B: ttgl.constexpr = shared_layouts[1]
@@ -220,24 +221,26 @@ def gemm_tdm_pipelined_warp_pipelined_kernel(a_ptr, b_ptr, c_ptr,  #
     accumulator = ttgl.zeros((BLOCK_M, BLOCK_N), dtype=c_ptr.type.element_ty, layout=WMMA_LAYOUT)
 
     # Triple buffering
-
-    # prefetch 3 first.
+    # prefetch 2, the other one is overlapped.
     for _ in ttgl.static_range(2):
         producer = issue_loads(producer, a_desc, b_desc, 0, 0, a_buffer, b_buffer, BLOCK_K, NUM_BUFFERS, TRANSPOSE_B)
 
-    ttgl.amd.gfx1250.tdm.async_wait((NUM_BUFFERS - 2) * 2)
+    # Wait for the first prefetch
+    ttgl.amd.gfx1250.tdm.async_wait(1 * 2)
     for _ in range(0, ttgl.cdiv(K, BLOCK_K) - (NUM_BUFFERS - 1)):
         with ttgl.amd.warp_pipeline_stage("stage0"):
             consumer, a, b = lds_load(consumer, a_buffer, OPERAND_LAYOUT_A, b_buffer, OPERAND_LAYOUT_B, NUM_BUFFERS,
                                       TRANSPOSE_B)
-
-        ttgl.amd.gfx1250.tdm.async_wait((NUM_BUFFERS - 2) * 2)
+        # Wait for the last one, either before the loop or a load below.
+        ttgl.amd.gfx1250.tdm.async_wait(0)
         with ttgl.amd.warp_pipeline_stage("stage1"):
             producer = issue_loads(producer, a_desc, b_desc, 0, 0, a_buffer, b_buffer, BLOCK_K, NUM_BUFFERS,
                                    TRANSPOSE_B)
             accumulator = issue_wmma_compute(a, b, accumulator)
 
     for i in ttgl.static_range(NUM_BUFFERS - 1):
+        # Warp-pipeline ended, wait for the ones to be consuned here.
+        ttgl.amd.gfx1250.tdm.async_wait((NUM_BUFFERS - 1 - i) * 2)
         consumer, accumulator = issue_wmma(consumer, a_buffer, OPERAND_LAYOUT_A, b_buffer, OPERAND_LAYOUT_B,
                                            accumulator, (NUM_BUFFERS - 2 - i) * 2, NUM_BUFFERS, TRANSPOSE_B)
 
@@ -255,12 +258,13 @@ def gemm_tdm_pipelined_warp_pipelined_kernel(a_ptr, b_ptr, c_ptr,  #
 @pytest.mark.parametrize("PREFETCH", [False])
 @pytest.mark.parametrize("M,N,K", [(2048, 2048, 2048)])
 @pytest.mark.parametrize("num_warps", [8])
+@pytest.mark.parametrize("DUMP", [False])
 def test_runtime_gemm_tdm_pipelined(BLOCK_M, BLOCK_N, BLOCK_K, NUM_BUFFERS, TRANSPOSE_B, PERSISTENT, PREFETCH, M, N, K,
-                                    num_warps):
+                                    num_warps, DUMP):
     if triton.cdiv(K, BLOCK_K) < NUM_BUFFERS:
         pytest.skip("Skip tests where K/BLOCK_K < NUM_BUFFERS")
 
-    #torch.manual_seed(42)
+    torch.manual_seed(42)
 
     a = torch.randn((M, K), dtype=torch.float16)
     b = torch.randn((K, N), dtype=torch.float16)
@@ -275,6 +279,16 @@ def test_runtime_gemm_tdm_pipelined(BLOCK_M, BLOCK_N, BLOCK_K, NUM_BUFFERS, TRAN
     b_device = b.cuda()
     c_device = c.cuda()
 
+    if num_warps == 4:
+        # warpsPerCTA = [2, 2]
+        WARP_BASES = [(0, 1), (1, 0)]
+    elif num_warps == 8:
+        # warpsPerCTA = [4, 2]
+        WARP_BASES = [(0, 1), (1, 0), (2, 0)]
+    else:
+        raise ValueError(f"unsupported num_warps={num_warps}")
+
+    warp_bases = tuple(WARP_BASES)
     if not PERSISTENT:
         grid = (triton.cdiv(M, BLOCK_M) * triton.cdiv(N, BLOCK_N), 1)
         kernel = gemm_tdm_pipelined_warp_pipelined_kernel[grid](
@@ -284,7 +298,7 @@ def test_runtime_gemm_tdm_pipelined(BLOCK_M, BLOCK_N, BLOCK_K, NUM_BUFFERS, TRAN
             stride_bk, stride_bn,  #
             stride_cm, stride_cn,  #
             BLOCK_M=BLOCK_M, BLOCK_N=BLOCK_N, BLOCK_K=BLOCK_K,  #
-            NUM_BUFFERS=NUM_BUFFERS, TRANSPOSE_B=TRANSPOSE_B, NUM_WARPS=num_warps,  #
+            NUM_BUFFERS=NUM_BUFFERS, TRANSPOSE_B=TRANSPOSE_B, WARP_BASES=warp_bases,  #
             num_warps=num_warps, waves_per_eu=num_warps // 4)
         static_profile(kernel)
     else:
@@ -297,6 +311,12 @@ def test_runtime_gemm_tdm_pipelined(BLOCK_M, BLOCK_N, BLOCK_K, NUM_BUFFERS, TRAN
     c_triton = c_device.cpu()
     c_torch = a.to(torch.float32) @ (b.to(torch.float32) if not TRANSPOSE_B else b.T.to(torch.float32))
     torch.testing.assert_close(c_triton, c_torch, rtol=1e-4, atol=1e-4)
+    if DUMP:
+        print("triton")
+        print(c_triton)
+        print("torch")
+        print(c_torch)
+        print("Done.")
 
 
 if __name__ == "__main__":
@@ -310,13 +330,10 @@ if __name__ == "__main__":
     parser.add_argument("--num-buffers", type=int, choices=[1, 2, 4], default=2, help='num shared memory buffers')
     parser.add_argument("--persistent", action="store_true", help="Use persistent variant")
     parser.add_argument("--prefetch-lds", action="store_true", help="Enable prefetch LDS")
-    parser.add_argument("--single-warp-schedule", action="store_true", help="Use single warp per SIMD schedule variant")
+    parser.add_argument("--dump", action="store_true", help="Print out result/golden tensors")
     args = parser.parse_args()
 
     assert not (args.persistent and args.single_warp_schedule)
-    if args.single_warp_schedule:
-        assert args.num_warps == 4
-        assert args.prefetch_lds
 
     M, N, K = args.M, args.N, args.K
     BLOCK_M, BLOCK_N, BLOCK_K = 256, 256, 64
@@ -325,10 +342,11 @@ if __name__ == "__main__":
     TRANSPOSE_B = True
     PERSISTENT = args.persistent
     PREFETCH = args.prefetch_lds
+    DUMP = args.dump
     print(
         f"({M=}, {N=}, {K=}), ({BLOCK_M=}, {BLOCK_N=}, {BLOCK_K=}), {TRANSPOSE_B=}, {NUM_WARPS=}, {NUM_BUFFERS=}, {PERSISTENT=}, {PREFETCH=}"
     )
 
     test_runtime_gemm_tdm_pipelined(BLOCK_M, BLOCK_N, BLOCK_K,  #
                                     3, True, False, False,  #
-                                    M, N, K, 8)
+                                    M, N, K, 8, DUMP)
