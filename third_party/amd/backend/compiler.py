773638be0b8d94186f7b68d2ff6c6e0bea8d4ec8
Lixun Zhang <lixun.zhang@amd.com>
2026-01-09 03:29:15 +0000

    Integrate amdgcnas into compiler.py

--- third_party/amd/backend/compiler.py
+++ third_party/amd/backend/compiler.py
@@ -11,6 +11,7 @@ import os
 import functools
 import warnings
 from pathlib import Path
+from triton.tools.amdgcnas import amdgcn_as
 
 
 def get_min_dot_size(target: GPUTarget):
@@ -490,6 +491,8 @@ class HIPBackend(BaseBackend):
                             dump_file_id)
         amdgcn = llvm.translate_to_asm(src, amd.TARGET_TRIPLE, options.arch, features, flags, options.enable_fp_fusion,
                                        False)
+
+        amdgcn = amdgcn_as(amdgcn, False)
 
         if "AMD_INSERT_AMDGCN" in os.environ.keys():
             insert_module_path = str(os.environ["AMD_INSERT_AMDGCN"])
