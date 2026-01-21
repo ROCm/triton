2f53bbe3ea37eb5e26715ddfc0ae7839fc3010fd
Mylonas, Panagiotis <Panagiotis.Mylonas@amd.com>
2026-01-21 15:58:49 +0100

    Enable FFM time slicing knob for MXFP GEMM (#465)

--- .github/workflows/gluon.sh
+++ .github/workflows/gluon.sh
@@ -55,9 +55,9 @@ HSA_MODEL_NUM_THREADS=2 pytest --count=1 -n 16 --durations=10 third_party/amd/py
 HSA_MODEL_NUM_THREADS=2 pytest --count=1 -n 16 --durations=10 third_party/amd/python/examples/gluon/f16_gemm_streamk_gfx1250.py
 HSA_MODEL_NUM_THREADS=2 pytest --count=1 -n 16 --durations=10 third_party/amd/python/examples/gluon/f16_fa_gfx1250.py
 
-# TODO: Fix failures in mxfp variants when ffm_enable_time_slicing is enabled.
-unset HSA_MODEL_ARGS
 HSA_MODEL_NUM_THREADS=4 pytest --count=1 -n 16 --durations=10 third_party/amd/python/examples/gluon/mxfp_gemm_gfx1250.py
+# TODO: Fix failure in test_block_scaled_attn_fwd[e4m3-e4m3-1-1024-1024-1-1-128-128-128-True-True-False-8-8] when ffm_enable_time_slicing is enabled.
+unset HSA_MODEL_ARGS
 HSA_MODEL_NUM_THREADS=4 pytest --count=1 -n 16 --durations=10 third_party/amd/python/examples/gluon/mxfp_fa_gfx1250.py
 export HSA_MODEL_ARGS=ffm_enable_time_slicing
 HSA_MODEL_NUM_THREADS=8 pytest --count=1 -n 4 --durations=10 third_party/amd/python/examples/gluon/test_kernel_metadata.py
