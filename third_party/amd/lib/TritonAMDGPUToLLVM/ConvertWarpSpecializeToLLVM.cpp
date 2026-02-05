4c98726b9f6698fc6498f5a7530530adc3d984a3
Zhang, Lei <Lei.Zhang2@amd.com>
2026-02-05 10:17:37 -0800

    Fix barrier lowering for GFX1250 (#517)
    Co-authored-by: pmylonas <Panagiotis.Mylonas@amd.com>
    Co-authored-by: Nicola Zaghen <Nicola.Zaghen@amd.com>

--- third_party/amd/lib/TritonAMDGPUToLLVM/ConvertWarpSpecializeToLLVM.cpp
+++ third_party/amd/lib/TritonAMDGPUToLLVM/ConvertWarpSpecializeToLLVM.cpp
@@ -90,17 +90,17 @@ static void createAllBarrier(TritonLLVMIRRewriter &b) {
 static LogicalResult rewriteWarpGroupBarriers(
     LLVM::LLVMFuncOp func, ArrayRef<WarpSpecializeOp> wsOps,
     unsigned defaultNumWarps, const AMD::TargetInfo &targetInfo) {
-  // HACK: Turn all `rocdl.barrier` ops into warp group barriers.
+
   func.walk<mlir::WalkOrder::PreOrder>([&](Operation *op) {
     // Walk into default regions but not partition regions.
     if (isa<WarpSpecializePartitionsOp>(op))
       return WalkResult::skip();
 
-    if (auto bar = dyn_cast<ROCDL::BarrierOp>(op)) {
-      TritonLLVMIRRewriter b(bar.getLoc(), bar);
+    if (isa<ROCDL::BarrierOp, ROCDL::SBarrierOp>(op)) {
+      TritonLLVMIRRewriter b(op->getLoc(), op);
       createBarrier(b, kDefaultWarpGroupBarrierIdx, defaultNumWarps,
                     targetInfo);
-      bar.erase();
+      op->erase();
       return WalkResult::skip();
     }
     return WalkResult::advance();
@@ -119,11 +119,13 @@ static LogicalResult rewriteWarpGroupBarriers(
                << " warp group partitions";
       }
 
-      partition->walk([&](ROCDL::BarrierOp bar) {
-        TritonLLVMIRRewriter b(bar.getLoc(), bar);
-        unsigned partitionNumWarps = op.getPartitionNumWarps()[idx];
-        createBarrier(b, barIdx, partitionNumWarps, targetInfo);
-        bar.erase();
+      partition->walk([&](Operation *barOp) {
+        if (isa<ROCDL::BarrierOp, ROCDL::SBarrierOp>(barOp)) {
+          TritonLLVMIRRewriter b(barOp->getLoc(), barOp);
+          unsigned partitionNumWarps = op.getPartitionNumWarps()[idx];
+          createBarrier(b, barIdx, partitionNumWarps, targetInfo);
+          barOp->erase();
+        }
       });
     }
   }
