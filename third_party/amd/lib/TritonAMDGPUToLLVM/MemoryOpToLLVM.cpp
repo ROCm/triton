c27c2d40b5ac9d9087160958571fb871b86a9b64
Lei Zhang <Lei.Zhang@amd.com>
2026-02-06 07:01:10 +0000

    Revert "Fix barrier lowering for GFX1250 (#517)"
    This reverts commit 4c98726b9f6698fc6498f5a7530530adc3d984a3.

--- third_party/amd/lib/TritonAMDGPUToLLVM/MemoryOpToLLVM.cpp
+++ third_party/amd/lib/TritonAMDGPUToLLVM/MemoryOpToLLVM.cpp
@@ -453,9 +453,9 @@ public:
   LogicalResult
   matchAndRewrite(triton::gpu::BarrierOp op, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const override {
-    if (targetInfo.getIsaVersion().Major < 9)
+    if (!isCDNA(targetInfo.getISAFamily()) ||
+        targetInfo.getISAFamily() == AMD::ISAFamily::GFX1250)
       return failure();
-
     // Check no other memory addrspaces are selected.
     // TensorRead/Write are allowed but noop.
     auto mask = triton::gpu::AddrSpace::Local |
@@ -552,10 +552,9 @@ struct MemoryCounterWaitOpConversion
   LogicalResult
   matchAndRewrite(amdgpu::MemoryCounterWaitOp op, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const override {
-    // amdgpu::MemoryCounterWaitOp supports gfx9 onwards
     auto isaVersion = targetInfo.getIsaVersion();
 
-    /// If major version >= gfx12, lower to
+    /// If major version >= fgx12, lower  to
     ///   * ROCDL::WaitDscntOp if ds is present
     ///   * ROCDL::WaitLoadcntOp if load is present
     ///   * ROCDL::WaitStorecntOp if store is present
