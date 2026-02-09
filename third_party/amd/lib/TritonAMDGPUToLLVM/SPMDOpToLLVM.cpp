cce70b93d7e9f4c4d023ff2cc00a9960c5d62d78
Zhang, Lei <Lei.Zhang2@amd.com>
2026-02-09 15:14:46 -0800

    Drop special GetNumProgramsOp lowering logic (#533)

--- third_party/amd/lib/TritonAMDGPUToLLVM/SPMDOpToLLVM.cpp
+++ third_party/amd/lib/TritonAMDGPUToLLVM/SPMDOpToLLVM.cpp
@@ -1,9 +1,7 @@
 #include "Dialect/TritonAMDGPU/IR/Dialect.h"
 #include "PatternTritonGPUOpToLLVM.h"
 #include "Utility.h"
-#include "mlir/Dialect/Arith/IR/Arith.h"
 #include "mlir/Dialect/LLVMIR/ROCDLDialect.h"
-#include "triton/Conversion/TritonGPUToLLVM/Utility.h"
 
 using namespace mlir;
 
@@ -16,29 +14,14 @@ struct GetNumProgramsOpConversion
   LogicalResult
   matchAndRewrite(triton::GetNumProgramsOp op, OpAdaptor adaptor,
                   ConversionPatternRewriter &rewriter) const override {
+    static constexpr mlir::gpu::Dimension dims[] = {mlir::gpu::Dimension::x,
+                                                    mlir::gpu::Dimension::y,
+                                                    mlir::gpu::Dimension::z};
     Location loc = op->getLoc();
-
-    // Get the num_program from the implicitarg ptr which might be lowered into
-    // directly loading it from user sgprs
-    // The first 12 bytes represent the hidden block count x,y,z
-    auto b = TritonLLVMOpBuilder(loc, rewriter);
-    auto ptrTy = ptr_ty(rewriter.getContext(), 4);
-    auto implicitArgPtr =
-        LLVM::createLLVMIntrinsicCallOp(
-            rewriter, loc, "llvm.amdgcn.implicitarg.ptr", {ptrTy}, {})
-            .getResult(0);
-
-    auto offset = b.i32_val(op.getAxisAsInt());
-    auto gridDimAxisPtr = b.gep(ptrTy, i32_ty, implicitArgPtr, offset);
-    rewriter.replaceOp(op, b.load(i32_ty, gridDimAxisPtr));
-
-    // TODO(alex) I think that is not true anymore, the same should work on gfx9
-    // TODO: this needs to stay for arch < MI400. Check the target info and
-    // assert(op.getAxisAsInt() < 3);
-    // Value blockId =
-    //     ::mlir::gpu::GridDimOp::create(rewriter, loc,
-    //     dims[op.getAxisAsInt()]);
-    // rewriter.replaceOpWithNewOp<arith::TruncIOp>(op, i32_ty, blockId);
+    assert(op.getAxisAsInt() < 3);
+    Value blockId =
+        ::mlir::gpu::GridDimOp::create(rewriter, loc, dims[op.getAxisAsInt()]);
+    rewriter.replaceOpWithNewOp<arith::TruncIOp>(op, i32_ty, blockId);
     return success();
   }
 };
