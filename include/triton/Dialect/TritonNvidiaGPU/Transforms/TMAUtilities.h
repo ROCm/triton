eb30cba37078f2ae83a822a018a290a64b6dd45b
Lei Zhang <Lei.Zhang2@amd.com>
2026-03-21 20:57:54 +0000

    Revert "[NFC] Refactor OptimizeDescriptorEncoding (#600)"
    This reverts commit 50ec4223143ed2997882c1af83b18f9616e56df9.

--- include/triton/Dialect/TritonNvidiaGPU/Transforms/TMAUtilities.h
+++ include/triton/Dialect/TritonNvidiaGPU/Transforms/TMAUtilities.h
@@ -16,6 +16,9 @@ inline bool isFp4Padded(Attribute encoding) {
   return mmaEnc && mmaEnc.getFp4Padded();
 }
 
+gpu::CGAEncodingAttr updateCGALayoutForShape(gpu::CGAEncodingAttr cgaLayout,
+                                             ArrayRef<int64_t> shape);
+
 gpu::SharedEncodingTrait
 updateEncodingForShape(Operation *op, gpu::SharedEncodingTrait encoding,
                        RankedTensorType tensorType);
