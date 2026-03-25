995f0c2a9e9ec3479b750b982cad677b94895d13
Lei Zhang <Lei.Zhang2@amd.com>
2026-03-25 06:03:09 +0000

    Add None for weight_dtype_str to checked MoE tests

--- .github/workflows/moe.sh
+++ .github/workflows/moe.sh
@@ -61,10 +61,10 @@ git apply .github/workflows/moe_triton_testing.patch
 
 # Due to time limit, we can only select a very limited set of tests to run.
 TRITON_MOE_TESTS=(
-    "test_op[None-False-False-True-True-None-128-768-512-1024-batched-float16-float16-10-1-False-False-None-False-False-False-True-None]"
-    "test_op[None-False-True-False-True-None-128-16-256-256-ragged-float8_e5m2-mxfloat4_e2m1-10-1-False-True-None-False-False-False-True-None]"
-    "test_op[None-False-True-False-True-None-128-300-400-832-ragged-float8_e5m2-mxfloat4_e2m1-10-1-False-False-None-False-False-False-True-None]"
-    "test_op[None-False-False-False-False-None-16-727-577-859-ragged-float16-float16-10-1-False-False-None-False-False-False-True-None]"
+    "test_op[None-False-False-True-True-None-128-768-512-1024-batched-float16-float16-None-10-1-False-False-None-False-False-False-True-None]"
+    "test_op[None-False-True-False-True-None-128-16-256-256-ragged-float8_e5m2-mxfloat4_e2m1-None-10-1-False-True-None-False-False-False-True-None]"
+    "test_op[None-False-True-False-True-None-128-300-400-832-ragged-float8_e5m2-mxfloat4_e2m1-None-10-1-False-False-None-False-False-False-True-None]"
+    "test_op[None-False-False-False-False-None-16-727-577-859-ragged-float16-float16-None-10-1-False-False-None-False-False-False-True-None]"
 )
 
 K_EXPR=""
