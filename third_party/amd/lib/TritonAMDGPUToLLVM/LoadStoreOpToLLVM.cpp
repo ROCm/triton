7c57a032d851d43ab375ebd7c5470823cab2a2f3
Efimov, Aleksandr <Aleksandr.Efimov@amd.com>
2026-04-20 13:44:26 +0200

    [AMD][GFX12] Cache modifiers for buffer and async ops
    This PR enables generation of cache policy/modifiers in buffer and async operations(loads and stores) on GFX12 architectures. This include RDNA4 and GFX1250 hardware.

--- third_party/amd/lib/TritonAMDGPUToLLVM/LoadStoreOpToLLVM.cpp
+++ third_party/amd/lib/TritonAMDGPUToLLVM/LoadStoreOpToLLVM.cpp
@@ -1001,9 +1001,6 @@ struct AsyncCopyGlobalToLocalOpConversion
       if (targetInfo.requiresAliasInfoForAsyncOps())
         AMD::addAsyncCopyAliasScope(asyncLoadOp);
     } else if (targetInfo.getISAFamily() == ISAFamily::GFX1250) {
-      if (cacheMod != triton::CacheModifier::NONE) {
-        emitRemark(loc) << "cache modifiers not yet implemented on gfx1250";
-      }
       switch (vecBits) {
       case 32:
         if (multicastMask)
@@ -1142,10 +1139,6 @@ struct AsyncCopyLocalToGlobalOpConversion
     int32_t cacheModifiers =
         mlir::LLVM::AMD::getCtrlBitsForCacheModifierOnTarget(
             cacheMod, /*isLoad=*/false, targetInfo);
-
-    if (cacheMod != triton::CacheModifier::NONE) {
-      emitRemark(loc) << "cache modifiers not yet implemented on gfx1250";
-    }
 
     auto emitStore = [&](int bits, Value dst, Value shmem) {
       std::string intrinsic =
