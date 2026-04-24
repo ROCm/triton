644f406aff700fce9007dc743ba716c9b5c174c4
Keles, Volkan <Volkan.Keles@amd.com>
2026-04-24 21:37:35 +0300

    [AMD][gfx1250] Add fused SwiGLU mode to gluon mxfp GEMM example (#702)

--- .github/workflows/gluon.sh
+++ .github/workflows/gluon.sh
@@ -76,7 +76,7 @@ HSA_MODEL_NUM_THREADS=2 pytest --count=1 -n 16 --durations=10 third_party/amd/py
 
 unset HSA_MODEL_ARGS
 # TODO: Fix failues in test_runtime_mxgemm_tdm_8warps_pipeline
-HSA_MODEL_NUM_THREADS=4 pytest --count=1 -n 32 --durations=10 third_party/amd/python/examples/gluon/mxfp_gemm_gfx1250.py
+HSA_MODEL_NUM_THREADS=4 pytest --count=1 -n 32 --forked --durations=10 third_party/amd/python/examples/gluon/mxfp_gemm_gfx1250.py
 # TODO: Fix failure in test_block_scaled_attn_fwd[e4m3-e4m3-1-1024-1024-1-1-128-128-128-True-True-False-8-8] when ffm_enable_time_slicing is enabled.
 # It seems ffm_enable_time_slicing gives incorrect numerics when >~70 VGPR spills.
 HSA_MODEL_NUM_THREADS=4 pytest --count=1 -n 16 --durations=10 third_party/amd/python/examples/gluon/f16_fa_gfx1250.py
